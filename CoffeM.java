15c6bb37074d5ebac0a1d159a8868cc728b780e7
Khizri Edilov <Javaoracle98@yandex.ru>
2020-04-28 14:32:31 +0300

    add Scanner

--- CoffeM.java
+++ CoffeM.java
@@ -1,11 +1,34 @@
-import java.lang.String;
+import java.util.Scanner;
 public class CoffeM {
 public static void main(String[]args){
-    int [] priceDrink = {240, 120,60,20};
-    String [] nameDrink = {"Kapuchino", "Espresso", "Wather", "Milk"};
-    int myMoney = 120;
-    System.out.println("Кофе машина");
-    
+    Scanner num = new Scanner(System.in);
+     int Kapuchino = 210;
+     int espresso  = 110;
+     int milk = 60;
+     int wather = 40;
+ int myMoney;
+    System.out.println("Сколько у вас денег?");
+    myMoney = num.nextInt();
+    if (myMoney >= Kapuchino){
+        System.out.println("Ваш капучино ");
+    } else {
+        System.out.println("У вас недостаточно средств для покупки капучино");
+    }
+    if (myMoney >= espresso){
+        System.out.println("Ваш эспрессо");
+    }else {
+        System.out.println("У вас недостаточно средств для покупки эспрессо");
+    }
+    if(myMoney >= milk){
+        System.out.println("Ваше молоко");
+    }else{
+        System.out.println("У вас недостаточно средств для покупки молока");
+    }
+    if(myMoney >= wather){
+        System.out.println("Ваша вода");
+    }else {
+        System.out.println("У вас недостаточно средств для покупки воды");
+    }
 
 }
 }
